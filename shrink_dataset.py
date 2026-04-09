5f345d98e7cf3266ec3e71a717240ee3ec7e648b
yujetak <tech.yuje@gmail.com>
2026-04-09 20:44:20 +0900

    fix

--- shrink_dataset.py
+++ shrink_dataset.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 df = pd.read_csv('./dataset/card_sales_summary.csv')
 
-df_small = df[df['std_ym'] >= 202301].copy()
+df_small = df[df['std_ym'] >= 202401].copy()
 df_small.to_csv('./dataset/card_sales_summary_small.csv', index=False)
 
-print("202301 이후 저장 완료")
+print("202401 이후 저장 완료")
